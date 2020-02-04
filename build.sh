1ac73eeedec0c623b52f80d69113bf3633e554d7
James Swineson <github@public.swineson.me>
2020-02-04 14:49:41 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-set -x
+set -Eexuo pipefail
 
 REVISION=${REVISION:-"latest"}
 
-git config --global --unset core.autocrlf
+git config --global --unset core.autocrlf || true
+
 wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 java -jar BuildTools.jar --rev "$REVISION" --output-dir "${BUILD_ARTIFACTSTAGINGDIRECTORY}"
 ls -alh "${BUILD_ARTIFACTSTAGINGDIRECTORY}"
